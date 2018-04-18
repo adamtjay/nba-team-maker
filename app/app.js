648c98fe4e1e5d6d0ff219eb8196079fa0d7ca2b
Adam Julier <adamjulier@Kevins-Air.home>
2018-04-18 18:36:40 -0400

    file structure

---
+++ app/app.js
@@ -0,0 +1,39 @@
+// Import dependencies
+const path = require('path');
+
+const express = require('express');
+const logger = require('morgan');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+const PORT = 3000;
+
+const app = express();
+
+// const nbaRouter = require('./routes/nbaRouter');
+
+
+// ***  Middleware
+app.use(logger('dev')); // Log request info to console
+app.use(bodyParser.urlencoded({extended: false})); // parse urlencoded req bodies (for POST and PUT requests)
+app.use(bodyParser.json()); // parse json req bodies (for POST and PUT requests)
+app.use(methodOverride('_method'));
+
+// *** View & static asset paths
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')))
+
+// *** Root route
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+// *** Router
+//app.use('/routerpath', nbaRouter);
+
+// Listen on PORT
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
