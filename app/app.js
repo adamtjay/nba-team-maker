591ab0afef74c89b3e10724f2e2deb1c17e76c23
Adam Julier <adamjulier@ool-ad020067.dyn.optonline.net>
2018-04-19 17:39:10 -0400

    player avatar imgs, layouts and css

--- app/app.js
+++ app/app.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 // *** Root route
 app.get('/', (req, res) => {
-  res.send('Hi there <br/><br/><a href="http://localhost:3000/nba">Go to NBA Maker app</a>');
+  res.send('<div style="text-align:center;padding:20px">Hi there <br/><br/><a href="http://localhost:3000/nba">Go to NBA Maker app</a></div>');
 });
 
 // *** Router
