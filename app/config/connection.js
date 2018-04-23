6fc4fef1be1a92e44d2e3bc188d887a7c89b0b02
Adam Julier <adamjulier@ool-ad020067.dyn.optonline.net>
2018-04-23 16:05:18 -0400

    heroku db

--- app/config/connection.js
+++ app/config/connection.js
@@ -2,6 +2,6 @@ const pgp = require('pg-promise')();
 
 const config = require('./dbConfig');
 
-const db = pgp(config);
+const db = pgp(process.env.DATABASE_URL || config);  //env for heroku
 
 module.exports = db;
